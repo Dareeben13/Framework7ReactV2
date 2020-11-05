31299d3ff7a7a4f5e06d8f82c1313e561f11e5e9
clintjeez <clintjeez@gmail.com>
2020-11-06 00:19:56 +0100

    Added sign up page , and fixed nquestion page style

--- src/pages/question.jsx
+++ src/pages/question.jsx
@@ -1,143 +1,124 @@
-import React, { useState } from 'react'
-import {
-    Page,
-    Link,
-    Swiper,
-    SwiperSlide,
-    Tabs,
-    Tab,
-    Range
-} from 'framework7-react';
+import React, { useState } from "react";
+import { Page, Link, Tabs, Tab, Range } from "framework7-react";
 
-import QuestionItem from '../components/question/question'
-import ProgressBar from '../components/progress-bar/progress-bar'
+import QuestionItem from "../components/question/question";
+import ProgressBar from "../components/progress-bar/progress-bar";
 
-import bag from '../assets/bag.jpg'
-import cup from '../assets/cup.jpg'
-import duck from '../assets/duck.jpg'
-import tools from '../assets/tools.jpg'
-import certificate from '../assets/certificate.jpg'
-import cap from '../assets/cap.jpg'
-import employee from '../assets/employee.jpg'
-import robot from '../assets/robot.jpg'
-import codebook1 from '../assets/codebook1.jpg'
+import bag from "../assets/bag.jpg";
+import cup from "../assets/cup.jpg";
+import duck from "../assets/duck.jpg";
+import tools from "../assets/tools.jpg";
+import certificate from "../assets/certificate.jpg";
+import cap from "../assets/cap.jpg";
+import employee from "../assets/employee.jpg";
+import robot from "../assets/robot.jpg";
+import codebook1 from "../assets/codebook1.jpg";
 
-import '../css/questionpage.scss'
-
-
+import "../css/questionpage.scss";
 
 export default () => {
-    const [width, setWidth] = useState(33.3)
+  const [width, setWidth] = useState(33.3);
 
-    const divStyle = {
-        width: "" + width + "%"
-    };
+  const divStyle = {
+    width: "" + width + "%",
+  };
 
-    const SetWidth = () => {
-        setWidth(width + 33.3)
-    }
+  const SetWidth = () => {
+    setWidth(width + 33.3);
+  };
 
-    return (
-        <Page name="question" className="question-container">
-            <ProgressBar divStyle={divStyle} />
-            <Tabs>
-                <Tab className="swiper-no-swiping" tabActive id="tab-1">
-                    <div className="items-container">
-                        <p className="text-wrapper">
-                            Why are you learning to code?
-                     </p>
-                        <Link tabLink="#tab-2">
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={certificate}
-                                text="For my current job"
-                            />
-                        </Link>
-                        <Link tabLink="#tab-2">
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={duck}
-                                text="Just for fun"
-                            />
-                        </Link>
-                        <Link tabLink="#tab-2">
+  return (
+    <Page name="question" className="question-container">
+      <ProgressBar divStyle={divStyle} />
+      <Tabs>
+        <Tab className="swiper-no-swiping" tabActive id="tab-1">
+          <div className="items-container">
+            <p className="text-wrapper">Why are you learning to code?</p>
+            <Link tabLink="#tab-2" className="item-link">
+              <QuestionItem
+                onClick={SetWidth}
+                image={certificate}
+                text="For my current job"
+              />
+            </Link>
+            <Link tabLink="#tab-2" className="item-link">
+              <QuestionItem
+                onClick={SetWidth}
+                image={duck}
+                text="Just for fun"
+              />
+            </Link>
+            <Link tabLink="#tab-2" className="item-link">
+              <QuestionItem
+                onClick={SetWidth}
+                image={cup}
+                text="To become a professional developer"
+              />
+            </Link>
+            <Link tabLink="#tab-2" className="item-link">
+              <QuestionItem
+                onClick={SetWidth}
+                image={tools}
+                text="For a different reason"
+              />
+            </Link>
+          </div>
+        </Tab>
+        <Tab id="tab-2">
+          <div className="items-container">
+            <p className="text-wrapper">Why are you learning to code?</p>
+            <Link tabLink="#tab-3" className="item-link">
+              <QuestionItem
+                onClick={SetWidth}
+                image={bag}
+                text="High school student"
+              />
+            </Link>
 
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={cup}
-                                text="To become a professional developer"
-                            />
-                        </Link>
-                        <Link tabLink="#tab-2">
+            <Link tabLink="#tab-3" className="item-link">
+              <QuestionItem
+                onClick={SetWidth}
+                image={cap}
+                text="University student"
+              />
+            </Link>
+            <Link tabLink="#tab-3" className="item-link">
+              <QuestionItem
+                onClick={SetWidth}
+                image={employee}
+                text="Employee"
+              />
+            </Link>
+            <Link tabLink="#tab-3" className="item-link">
+              <QuestionItem onClick={SetWidth} image={robot} text="Other" />
+            </Link>
+          </div>
+        </Tab>
+        <Tab id="tab-3" className="tab-3">
+          <div className="tab3-wrapper">
+            <p className="tab3-text">How much coding experience do you have?</p>
 
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={tools}
-                                text="For a different reason"
-                            />
-                        </Link>
-                    </div>
-                </Tab>
-                <Tab id="tab-2">
-                    <div className="items-container">
-                        <p className="text-wrapper">
-                            Why are you learning to code?
-                     </p>
-                        <Link tabLink="#tab-3">
+            <div className="tab3-image">
+              <img src={codebook1} alt="books" />
+            </div>
 
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={bag}
-                                text="High school student"
-                            />
-                        </Link>
-
-                        <Link tabLink="#tab-3">
-
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={cap}
-                                text="University student"
-                            />
-                        </Link>
-                        <Link tabLink="#tab-3">
-
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={employee}
-                                text="Employee"
-                            />
-                        </Link>
-                        <Link tabLink="#tab-3">
-
-                            <QuestionItem
-                                onClick={SetWidth}
-                                image={robot}
-                                text="Other"
-                            />
-                        </Link>
-                    </div>
-                </Tab>
-                <Tab id="tab-3" className="tab-3">
-                    <div className="tab3-wrapper">
-                        <p className="tab3-text">How much coding experience do you have?</p>
-
-                        <div className="tab3-image">
-                            <img src={codebook1} alt="books" />
-                        </div>
-
-                        <Range
-                            min={0}
-                            max={10}
-                            label={true}
-                            step={5}
-                            value={0}
-                            draggableBar={true}
-                        >
-                        </Range>
-                    </div>
-                </Tab>
-            </Tabs>
-        </Page>
-    )
-}
+            {/* <Range
+              min={0}
+              max={10}
+              label={true}
+              step={5}
+              value={0}
+              draggableBar={true}
+            ></Range>
+            <p className="txt">You've written small programs or webpages</p> */}
+            <div className="link-container">
+              <Link className="link" href="/signup">
+                Continue
+              </Link>
+            </div>
+          </div>
+        </Tab>
+      </Tabs>
+    </Page>
+  );
+};
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React from "react";
 import {
-    Page,
-    Navbar,
-    NavTitle,
-    NavTitleLarge,
-    Link,
-    Toolbar,
-    Block,
-} from 'framework7-react';
+  Page,
+  Navbar,
+  NavTitle,
+  NavTitleLarge,
+  Link,
+  Toolbar,
+  Block,
+} from "framework7-react";
+
+import signupImg from "../assets/friends.jpg";
+import fbImg from "../assets/facebook.svg";
+import googleImg from "../assets/google.svg";
+
+import "../css/signup.scss";
 
 export default () => (
-    <Page name="signup">
-        Signup
-        <Navbar large>
-            <NavTitle>Mimo</NavTitle>
-            <NavTitleLarge>Mimo</NavTitleLarge>
-        </Navbar>
-    </Page>
-); 
+  <Page name="signup" className="signup">
+    <header className="signup-header">
+      <a href="#">Log in</a>
+    </header>
+    <h2 className="signup-title">
+      Create a profile to save your personal curriculum
+    </h2>
+    <div className="signup-imgWrapper">
+      <img className="signup-img" src={signupImg} alt="signup" />
+    </div>
+
+    <div className="signup-container">
+      <p className="signup-txt">
+        By signin up your agree to our <a href="#">terms</a> and{" "}
+        <a href="#">Privacy policy</a>
+      </p>
+
+      <button className="signup-btn fb">
+        <div className="icon-wrapper">
+          <img className="signup-icon" src={fbImg} alt="facebook" />
+        </div>
+        <p>continue with facebook</p>
+      </button>
+      <button className="signup-btn google">
+        <div className="icon-wrapper">
+          <img className="signup-icon" src={googleImg} alt="google" />
+        </div>
+        <p>continue with google</p>
+      </button>
+
+      <button className="signup-email">Sign up with email</button>
+    </div>
+  </Page>
+);
